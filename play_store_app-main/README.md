65cc279e161a3cb7ef6e77e26b949548c7cc279d
parththummar2060 <115215700+parththummar2060@users.noreply.github.com>
2024-01-10 16:43:51 +0530

    Add files via upload

---
+++ play_store_app-main/README.md
@@ -0,0 +1,8 @@
+# play_store_app
+
+A new Flutter project.
+
+## MY WORK
+
+![WhatsApp Video 2022-06-19 at 10 23 01 PM](https://user-images.githubusercontent.com/102571795/174491879-a5b75fcb-ea2f-401a-a482-93f425135483.gif)
+
---
+++ play_store_app-main/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:projectplaystore/pages/androidpage/homepage.dart';
+import 'package:projectplaystore/pages/iospage/ihomepage.dart';
+import 'library/source.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  Timer.periodic(const Duration(milliseconds: 100), (timer) { setState(() {});});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return (isIos)
+        ?CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        "/": (context) => const IHomePage(),
+        "game_page": (context) => const GamesPage(),
+        "app_page": (context) => const AppPage(),
+      },
+        )
+        :MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        "/": (context) => const HomePage(),
+        "install_page": (context) => const InstallPage(),
+      },
+    );
+  }
+}
+
